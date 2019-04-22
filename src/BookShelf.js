262ad92883bb9728aa01e99d075a4bd2078bbf02
Rodrigo Lemos <rodrigolemosrl@hotmail.com>
2019-04-22 19:25:47 -0300

    Update BookShelf.js

--- src/BookShelf.js
+++ src/BookShelf.js
@@ -2,21 +2,20 @@ import React, { Component } from 'react';
 import Book from './Book';
 
 class BookShelf extends Component {
-
-    constructor(props) {
-        super(props);
-        this.state = {};
-    }
-
     render() {
         return (
             <div className="bookshelf">
                 <h2 className="bookshelf-title">{this.props.section}</h2>
                 <div className="bookshelf-books">
                     <ol className="books-grid">
-                        {this.props.books.map( book => (
-                            <Book key={book.id} book={book} />
-                        ))}
+                     {(this.props.books.length) ? (
+                            this.props.books.map( book => (
+                                <Book key={book.id} book={book}
+                                changeBookShelf={this.props.changeBookShelf} />
+                            ))
+                        ) : (
+                            <span> There are no books in this section. </span>
+                        )}
                     </ol>
                 </div>
             </div>
