e52f0dbbee53b8ac87e81a5602852a573a57bd19
Rodrigo Lemos <rodrigolemosrl@hotmail.com>
2019-04-24 17:54:15 -0300

    Update Book.js

--- src/Book.js
+++ src/Book.js
@@ -5,23 +5,39 @@ class Book extends Component {
     constructor(props) {
         super(props);
         this.handleChange = this.handleChange.bind(this);
-        this.style = {
-            width: 128,
-            height: 193,
-            backgroundImage: `url(${this.props.book.imageLinks.thumbnail})`
-        }
     }
 
+    /**
+     * Evento atribuido ao onChange do select para
+     * trocar livro de prateleira
+     */
     handleChange(event) {
         this.props.changeBookShelf(this.props.book, event.target.value);
     }
 
     render() {
+
+        /**
+         * Se livro não possuir autor, atribui autor desconhecido
+         */
+        if ( typeof this.props.book.authors === 'undefined' || this.props.book.authors.length === 0 ) {
+            this.props.book.authors = ['Unknown Author'];
+        }
+
+        /**
+         * Somente atribui imagem do livro caso exista
+         */
+        const style = { width: 128, height: 193 };
+        let images = this.props.book.imageLinks;
+        if ( typeof images !== 'undefined' ) {
+            style.backgroundImage = `url(${images.thumbnail})`
+        }
+
         return (
             <li>
                 <div className="book">
                     <div className="book-top">
-                    <div className="book-cover" style={this.style}></div>
+                    <div className="book-cover" style={style}></div>
                     <div className="book-shelf-changer">
                         <select defaultValue={(this.props.book.shelf) ? this.props.book.shelf : 'none'} onChange={this.handleChange} >
                             <option value="move" disabled>Move to...</option>
